0a0e334c587280f6e0ee5ccfd59ac2c332bfdf63
G-Krushna <gkp.achary@gmail.com>
2026-05-28 15:14:50 +0530

    Implement simple interest calculation in q13.kt

---
+++ q13.kt
@@ -0,0 +1,11 @@
+// Calculate simple interest
+
+fun main() {
+    val principal = 5000
+    val rate = 5
+    val time = 2
+
+    val simpleInterest = (principal * rate * time) / 100
+
+    println("Simple Interest = $simpleInterest")
+}
