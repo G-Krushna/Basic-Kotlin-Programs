4dbcadfcfbeeb2e8141c08097df9453148cbc1b0
G-Krushna <gkp.achary@gmail.com>
2026-05-28 15:09:28 +0530

    Implement number swapping using a third variable

---
+++ q12_a.kt
@@ -0,0 +1,15 @@
+// Swap two numbers using third variable
+
+fun main() {
+   
+    var a = 10
+    var b = 20
+
+    var temp = a
+    a = b
+    b = temp
+
+    println("Using third variable:")
+    println("a = $a")
+    println("b = $b")
+}
