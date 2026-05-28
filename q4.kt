4720222d7a445e6a547b92d805b3de6e269e1505
G-Krushna <gkp.achary@gmail.com>
2026-05-28 14:45:19 +0530

    Add Kotlin program to reverse a number

---
+++ q4.kt
@@ -0,0 +1,14 @@
+// Reverse a number using loops
+
+fun main() {
+    var num = 12345
+    var reverse = 0
+
+    while (num != 0) {
+        val digit = num % 10
+        reverse = reverse * 10 + digit
+        num /= 10
+    }
+
+    println("Reversed Number = $reverse")
+}
