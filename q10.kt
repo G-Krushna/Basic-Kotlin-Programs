34c966b9ec556f555998c92e7873e52166060ea2
G-Krushna <gkp.achary@gmail.com>
2026-05-28 14:57:25 +0530

    Implement character classification for vowels and consonants

---
+++ q10.kt
@@ -0,0 +1,11 @@
+// Check whether character is vowel or consonant
+
+fun main() {
+    val ch = 'e'
+
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+        println("$ch is a Vowel")
+    } else {
+        println("$ch is a Consonant")
+    }
+}
